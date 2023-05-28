c547021e8baa674b15ecad7fd5e1d95663ba8986
zjh <1152937237@qq.com>
2023-05-29 00:00:20 +0800

    remove database

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,11 @@
-from config.database import HOST, PORT, USER, PASSWORD, DATABASE
-import pymysql
+# from config.database import HOST, PORT, USER, PASSWORD, DATABASE
+# import pymysql
 
-db = pymysql.Connect(
-        host=HOST,
-        port=PORT,
-        user=USER,
-        passwd=PASSWORD,
-        db=DATABASE,
-        charset='utf8'
-)
+# db = pymysql.Connect(
+#         host=HOST,
+#         port=PORT,
+#         user=USER,
+#         passwd=PASSWORD,
+#         db=DATABASE,
+#         charset='utf8'
+# )
--- models/model_issue.py
+++ models/model_issue.py
@@ -1,5 +1,5 @@
-from config.database import HOST, PORT, USER, PASSWORD, DATABASE
-import pymysql
+# from config.database import HOST, PORT, USER, PASSWORD, DATABASE
+# import pymysql
 import datetime
 import logging
 
@@ -7,10 +7,10 @@ utc_format = '%Y-%m-%dT%H:%M:%SZ'
 
 class Issue(object):
 
-    # 析构函数，断开数据库连接
-    def __del__(self):
-        self.cursor.close()
-        self.db.close()
+    # # 析构函数，断开数据库连接
+    # def __del__(self):
+    #     self.cursor.close()
+    #     self.db.close()
 
 
     # 构造函数，初始化并连接数据库
@@ -22,63 +22,63 @@ class Issue(object):
         fh.setFormatter(fmt)
         fh.setLevel(Flevel)
         self.logger.addHandler(fh)
-        try:
-            self.db = pymysql.Connect(
-                host=HOST,
-                port=PORT,
-                user=USER,
-                passwd=PASSWORD,
-                db=DATABASE,
-                charset='utf8')
-            self.cursor = self.db.cursor()
-        except Exception as e:
-            logging.error("Database connect error:%s" % e)
+        # try:
+        #     self.db = pymysql.Connect(
+        #         host=HOST,
+        #         port=PORT,
+        #         user=USER,
+        #         passwd=PASSWORD,
+        #         db=DATABASE,
+        #         charset='utf8')
+        #     self.cursor = self.db.cursor()
+        # except Exception as e:
+        #     logging.error("Database connect error:%s" % e)
 
 
     # 批量保存issues
-    def save_all(self, issue_list):
+    # def save_all(self, issue_list):
 
-        for issue in issue_list:
-            sql = """INSERT INTO issue(title, source, type, No, opened_time, latest_time, comment_number, answered, status, author, link, content)
-                             values ('%s', '%s', '%s','%s', '%s', '%s', '%s', '%s','%s', '%s', '%s')""" % (
-            issue['title'], issue['source'], issue['type'], issue['id'], issue['opened_time'], issue['latest_time'], issue['comment_number'], issue['answered'], issue['status'], issue['link'], issue['content'])
+    #     for issue in issue_list:
+    #         sql = """INSERT INTO issue(title, source, type, No, opened_time, latest_time, comment_number, answered, status, author, link, content)
+    #                          values ('%s', '%s', '%s','%s', '%s', '%s', '%s', '%s','%s', '%s', '%s')""" % (
+    #         issue['title'], issue['source'], issue['type'], issue['id'], issue['opened_time'], issue['latest_time'], issue['comment_number'], issue['answered'], issue['status'], issue['link'], issue['content'])
 
-            try:
-                self.cursor.execute(sql)
-                self.db.commit()
-            except:
-                try:  # 插入失败表示数据库存在次issue，转为update更新
-                    sql_update = """UPDATE issue SET title='%s', type='%s', opened_time='%s', latest_time='%s', comment_number='%s', answered='%s', status='%s', content='%s' WHERE No='%s'""" % (
-                    issue['title'], issue['type'], issue['opened_time'], issue['latest_time'], issue['comment_number'], issue['answered'], issue['status'], issue['content'], issue['id'])
-                    self.cursor.execute(sql_update)
-                    self.db.commit()
-                    print("更新完成")
-                except Exception as e:
-                    logging.error("update error:%s" % e)
-                    self.db.rollback()
+    #         try:
+    #             self.cursor.execute(sql)
+    #             self.db.commit()
+    #         except:
+    #             try:  # 插入失败表示数据库存在次issue，转为update更新
+    #                 sql_update = """UPDATE issue SET title='%s', type='%s', opened_time='%s', latest_time='%s', comment_number='%s', answered='%s', status='%s', content='%s' WHERE No='%s'""" % (
+    #                 issue['title'], issue['type'], issue['opened_time'], issue['latest_time'], issue['comment_number'], issue['answered'], issue['status'], issue['content'], issue['id'])
+    #                 self.cursor.execute(sql_update)
+    #                 self.db.commit()
+    #                 print("更新完成")
+    #             except Exception as e:
+    #                 logging.error("update error:%s" % e)
+    #                 self.db.rollback()
 
 
     # 保存一条issue记录
-    def save_one(self, issue):
-        sql = """INSERT INTO issue(title, source, type, No, opened_time, latest_time, comment_number, answered, status, author, link, content)
-                                         values('%s', '%s', '%s','%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')""" % (
-            issue['title'], issue['source'], issue['type'], issue['id'], issue['opened_time'], issue['latest_time'], issue['comment_number'], issue['answered'], issue['status'], issue['author'], issue['link'], issue['content'])
+    # def save_one(self, issue):
+    #     sql = """INSERT INTO issue(title, source, type, No, opened_time, latest_time, comment_number, answered, status, author, link, content)
+    #                                      values('%s', '%s', '%s','%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')""" % (
+    #         issue['title'], issue['source'], issue['type'], issue['id'], issue['opened_time'], issue['latest_time'], issue['comment_number'], issue['answered'], issue['status'], issue['author'], issue['link'], issue['content'])
 
-        try:
-            self.cursor.execute(sql)
-            self.db.commit()
-            print("%s : Insert successfully" % issue['id'])
-        except Exception as e:
-            try:  # 插入失败表示数据库存在次issue，转为update更新
-                print("Exception: %s" % e)
-                sql_update = """UPDATE issue SET title='%s', type='%s', opened_time='%s', latest_time='%s', comment_number='%s', answered='%s', status='%s', content='%s' WHERE No='%s'""" % (
-                    issue['title'], issue['type'], issue['opened_time'], issue['latest_time'], issue['comment_number'], issue['answered'], issue['status'], issue['content'], issue['id'])
-                self.cursor.execute(sql_update)
-                self.db.commit()
-                print("%s : Update successfully" % issue['id'])
-            except Exception as e:
-                logging.error("update error:%s" % e)
-                self.db.rollback()
+    #     try:
+    #         self.cursor.execute(sql)
+    #         self.db.commit()
+    #         print("%s : Insert successfully" % issue['id'])
+    #     except Exception as e:
+    #         try:  # 插入失败表示数据库存在次issue，转为update更新
+    #             print("Exception: %s" % e)
+    #             sql_update = """UPDATE issue SET title='%s', type='%s', opened_time='%s', latest_time='%s', comment_number='%s', answered='%s', status='%s', content='%s' WHERE No='%s'""" % (
+    #                 issue['title'], issue['type'], issue['opened_time'], issue['latest_time'], issue['comment_number'], issue['answered'], issue['status'], issue['content'], issue['id'])
+    #             self.cursor.execute(sql_update)
+    #             self.db.commit()
+    #             print("%s : Update successfully" % issue['id'])
+    #         except Exception as e:
+    #             logging.error("update error:%s" % e)
+    #             self.db.rollback()
 
 
     # 获取对应源的issue信息
@@ -143,33 +143,33 @@ class Issue(object):
 
 
     # select语句封装
-    def select(self, table, column, source, condition):
-        if condition == "":
-            sql = "SELECT %s FROM %s WHERE source=%s" % (column, table, source)
-        else:
-            sql = "SELECT %s FROM %s WHERE source=%s and %s" % (column, table, source, condition)
+    # def select(self, table, column, source, condition):
+    #     if condition == "":
+    #         sql = "SELECT %s FROM %s WHERE source=%s" % (column, table, source)
+    #     else:
+    #         sql = "SELECT %s FROM %s WHERE source=%s and %s" % (column, table, source, condition)
 
-        try:
-            self.cursor.execute(sql)
-            result = self.cursor.fetchall()
-            return result
-        except Exception as e:
-            print("Error occur: %s" % e)
+    #     try:
+    #         self.cursor.execute(sql)
+    #         result = self.cursor.fetchall()
+    #         return result
+    #     except Exception as e:
+    #         print("Error occur: %s" % e)
 
 
     # 计数函数封装
-    def count(self, table, source, condition=""):
-        if condition == "":
-            sql = "SELECT count(1) FROM %s WHERE source=%s" % (table, source)
-        else:
-            sql = "SELECT count(1) FROM %s WHERE source=%s and %s" % (table, source, condition)
-        res = 0
+    # def count(self, table, source, condition=""):
+    #     if condition == "":
+    #         sql = "SELECT count(1) FROM %s WHERE source=%s" % (table, source)
+    #     else:
+    #         sql = "SELECT count(1) FROM %s WHERE source=%s and %s" % (table, source, condition)
+    #     res = 0
 
-        try:
-            self.cursor.execute(sql)
-            result = self.cursor.fetchall()
-            res = result[0][0]
-        except Exception as e:
-            print("Error: fetch data error with message: %s" % e)
+    #     try:
+    #         self.cursor.execute(sql)
+    #         result = self.cursor.fetchall()
+    #         res = result[0][0]
+    #     except Exception as e:
+    #         print("Error: fetch data error with message: %s" % e)
 
-        return res
+    #     return res
